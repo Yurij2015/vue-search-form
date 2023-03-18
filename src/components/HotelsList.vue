86227cbb21e88c3b280398b6a13ffff95f7c2942
Yurij M <mokruy9999@gmail.com>
2023-03-18 23:37:25 +0100

    show hotels tables

--- src/components/HotelsList.vue
+++ src/components/HotelsList.vue
@@ -1,8 +1,10 @@
-<script setup>
+<script setup lang="ts">
 import axios from 'axios'
 import {ref, onMounted} from 'vue';
+import {ElTable} from 'element-plus'
+import 'element-plus/es/components/message/style/css'
 
-let hotels = ref(null);
+let hotels: Hotel[] = ref(null);
 
 onMounted(() => {
   axios
@@ -11,15 +13,60 @@ onMounted(() => {
         hotels.value = response.data.data
       })
 });
+
+interface Hotel {
+  name: string
+  price: number
+  bedrooms: number
+  bathrooms: number
+  storeys: number
+  garages: number
+}
+
+const tableRowClassName = ({row, rowIndex,}: {
+  row: Hotel
+  rowIndex: number
+}) => {
+  if (isOdd(rowIndex)) {
+    return 'warning-row'
+  } else if (!isOdd(rowIndex)) {
+    return 'success-row'
+  }
+  return ''
+}
+
+function isOdd(numb) {
+  let number = numb;
+  if (Math.floor(number / 2) == number / 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 </script>
 
 <template>
-  <div v-for="hotel in hotels">
-    <p>{{ hotel.name }}</p>
-    <p>{{ hotel.price }}</p>
-    <p>{{ hotel.bedrooms }}</p>
-    <p>{{ hotel.bathrooms }}</p>
-    <p>{{ hotel.storeys }}</p>
-    <p>{{ hotel.garages }}</p>
-  </div>
+
+  <el-table
+      :data="hotels"
+      style="width: 100%"
+      :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="name" label="Name"/>
+    <el-table-column prop="price" label="Price"/>
+    <el-table-column prop="bedrooms" label="Bedrooms"/>
+    <el-table-column prop="bathrooms" label="Bathrooms"/>
+    <el-table-column prop="storeys" label="Storeys"/>
+    <el-table-column prop="garages" label="Garages"/>
+  </el-table>
 </template>
+<style>
+.el-table .warning-row {
+  --el-table-tr-bg-color: var(--el-color-warning-light-9);
+}
+
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
