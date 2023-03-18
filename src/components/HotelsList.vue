ad2c40e11309e9f0bff1767bb2192d76b8ad326f
Yurij M <mokruy9999@gmail.com>
2023-03-18 22:50:52 +0100

    show hotels list

---
+++ src/components/HotelsList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import axios from 'axios'
+import {ref, onMounted} from 'vue';
+
+let hotels = ref(null);
+
+onMounted(() => {
+  axios
+      .get('http://localhost/api/hotels')
+      .then((response) => {
+        hotels.value = response.data.data
+      })
+});
+</script>
+
+<template>
+  <div v-for="hotel in hotels">
+    <p>{{ hotel.name }}</p>
+    <p>{{ hotel.price }}</p>
+    <p>{{ hotel.bedrooms }}</p>
+    <p>{{ hotel.bathrooms }}</p>
+    <p>{{ hotel.storeys }}</p>
+    <p>{{ hotel.garages }}</p>
+  </div>
+</template>
